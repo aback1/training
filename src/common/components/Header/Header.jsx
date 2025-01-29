033f1417b81773a54aa22a7518ead876b17a0bb6
aback <162435971+aback1@users.noreply.github.com>
2025-01-29 10:22:48 +0100

    Update Header.jsx

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="flex items-center bg-blue-100 p-5 ">
             <Icon name="Logo" ending="png" className="w-12 h-12 mr-4" />
-            <h1 className="text-2xl text-black">Trainings Projekt für Check24 Kennenlerntag</h1>
+            <h1 className="text-2xl text-black">Trainings Projekt Food Ordering</h1>
         </div>
     );
 }
